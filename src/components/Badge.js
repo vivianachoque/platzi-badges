a61d4540986ff3827d855ded8d1111b64054b8be
vivianachoque <vivianaachoque@gmail.com>
2021-01-18 19:03:16 -0300

    agrego logo de conferencia

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
+import confLogo from "../components/images/starweaver.png"
 
-class Badge extends React.Component{
-    render(){
-        return <h1>Badge</h1>;
-        }
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia"></img>
+        </div>
+
+        <div>
+          <h1>Viviana Choque</h1>
+        </div>
+
+        <div>
+          <p>Front-End Dev</p>
+          <p>@anytwitter</p>
+        </div>
+      </div>
+    );
+  }
 }
 export default Badge;
